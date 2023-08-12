ab0187df3bb2e38211751bc3fd6baf2ac67eac9c
Line-em <alineemily@hotmail.com>
2023-08-12 15:24:35 +0100

    modifying the lock icons so it can unlock

---
+++ src/assets/svgs/Unlock.jsx
@@ -0,0 +1,16 @@
+export default function Unlock(props) {
+	return (
+		<svg
+			width="1em"
+			height="1em"
+			viewBox="0 0 24 24"
+			xmlns="http://www.w3.org/2000/svg"
+			{...props}
+		>
+			<path
+				fill="var(--black)"
+				d="M16 8c1.1 0 2 .9 2 2v10c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V10c0-1.1.9-2 2-2h9V6c0-1.7-1.3-3-3-3S7 4.3 7 6H5c0-2.8 2.2-5 5-5s5 2.2 5 5v2h1m-6 9c1.1 0 2-.9 2-2s-.9-2-2-2s-2 .9-2 2s.9 2 2 2m12-4h-2V7h2v6m0 4h-2v-2h2v2Z"
+			></path>
+		</svg>
+	);
+}
