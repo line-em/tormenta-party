00371de466e5d9a1655b3dc40f6df07d9815ca3e
Line-em <alineemily@hotmail.com>
2023-08-12 13:43:46 +0100

    created floating input style

--- src/components/CharacterSheet/CharacterSheet.jsx
+++ src/components/CharacterSheet/CharacterSheet.jsx
@@ -2,6 +2,7 @@ import SectionHeading from "../Headings/SectionHeading";
 import styles from "./CharacterSheet.module.css";
 import Attributes from "@/components/Attributes/Attributes";
 import BasicInfo from "./BasicInfo";
+import "@/styles/floating-input.css";
 
 export default function CharacterSheet({ characterSheet }) {
 	return (
