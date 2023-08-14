0fc6221c20452dac6dab65f1f24e465f4e08f876
Line-em <alineemily@hotmail.com>
2023-08-14 09:55:12 +0100

    further fixes on the form, added watch temporarily to keep track of changes

---
+++ src/components/Form/NumberInput.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import { isNumberKey } from "@/app/utils";
+const NumberInput = ({ rowCss, inputCss, id, label, disabled, type, initialData }) => {
+	// FIXME: remove watch
+	const {
+		register,
+		watch,
+		formState: { errors }
+	} = useForm({
+		mode: "onChange",
+		defaultValues: {
+			[id]: initialData
+		}
+	});
+	// FIXME: remove watch
+	const watchAllFields = watch();
+	console.log(watchAllFields);
+
+	return (
+		<div className={`${rowCss} floating-label`}>
+			<input
+				type="number"
+				id={id}
+				className={`${inputCss} floating-input`}
+				placeholder=" "
+				disabled={disabled}
+				{...register(id, {
+					maxLength: 3,
+					min: 0,
+					max: 100,
+					required: true,
+					valueAsNumber: true,
+					validate: (value) => isNumberKey(value),
+					pattern: {
+						value: /^[0-9]*[1-9][0-9]*$/,
+						message: "Input must be a number greater than 0."
+					}
+				})}
+			/>
+			<label htmlFor={id} className={`${inputCss} floating-text`}>
+				{label}
+			</label>
+		</div>
+	);
+};
+
+export default NumberInput;
