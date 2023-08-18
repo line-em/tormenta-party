5fe24e3ba80a71687486cadb7eae68499949d01a
Line-em <alineemily@hotmail.com>
2023-08-18 23:25:42 +0100

    to test

---
+++ src/app/ficha/[name]/geral/page.jsx
@@ -0,0 +1,5 @@
+import Geral from "@/components/CharacterSheet/Geral/Geral";
+
+export default function Page({ charSheet }) {
+	return <Geral data={charSheet} />;
+}
---
+++ src/app/ficha/[name]/magic/page.js
@@ -0,0 +1,8 @@
+import Magias from "@/components/CharacterSheet/Magias/Magias";
+import React from "react";
+
+const Page = () => {
+	return <Magias />;
+};
+
+export default Page;
