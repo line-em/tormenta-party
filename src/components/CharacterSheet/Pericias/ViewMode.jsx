eafb93243f34451e0d05fb9d00aacfc00be403e4
Line-em <alineemily@hotmail.com>
2023-08-31 22:58:41 +0100

    a small start

--- src/components/CharacterSheet/Pericias/ViewMode.jsx
+++ src/components/CharacterSheet/Pericias/ViewMode.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import styles from "./Pericias.module.css";
 
-const ViewMode = ({ pericias, total }) => {
+const ViewMode = ({ pericias, attributes, levelBonus }) => {
+	const getTotal = (attr) => {
+		return Number(attr) + Number(levelBonus);
+	};
+
 	return (
 		<>
-			{pericias.map((item, i) => (
-				<React.Fragment key={item.skill}>
-					<li>{item.skill}</li>
+			{Object.keys(pericias).map((item) => (
+				<React.Fragment key={item}>
+					<li>{item}</li>
 					<li>
-						<strong>{total[item.skill]}</strong>
+						<strong>{getTotal(attributes[pericias[item]])}</strong>
 					</li>
 				</React.Fragment>
 			))}
